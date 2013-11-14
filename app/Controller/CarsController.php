02de11e09d5e32b24ca8d79602581b0f52093ec4
jsstrn <jessternrays@gmail.com>
2013-11-15 00:09:46 +0800

    resolved the edit() bug

--- app/Controller/CarsController.php
+++ app/Controller/CarsController.php
@@ -38,31 +38,27 @@ class CarsController extends AppController {
 	}
 
 	public function edit($id = null) {
+	    if (!$id) {
+	        throw new NotFoundException(__('Invalid Request'));
+	    }
 
-		if (!$id) {
-			throw new NotFoundException(__('Invalid Request'));
-		}
+	    $car = $this->Car->findById($id);
+	    if (!$car) {
+	        throw new NotFoundException(__('Invalid Request'));
+	    }
 
-		$car = $this->Car->findById('$id');
+	    if ($this->request->is(array('post', 'put'))) {
+	        $this->Car->id = $id;
+	        if ($this->Car->save($this->request->data)) {
+	            $this->Session->setFlash(__('Your car has been updated.'));
+	            return $this->redirect(array('action' => 'index'));
+	        }
+	        $this->Session->setFlash(__('Unable to update your car details.'));
+	    }
 
-		if (!$car) {
-			throw new NotFoundException(__('Invalid Request'));
-		}
-
-		if ($this->request->is('post') || $this->request->is('put')) {
-			$this->Car->id = $id;
-
-			if ($this->Car->save($this->request->data)) {
-				$this->Session->setFlash('Your car details have been updated.');
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash('Unable to update your car details.');
-			}
-		}
-
-		if (!$this->request->data) {
-			$this->request->data = $car;
-		}
+	    if (!$this->request->data) {
+	        $this->request->data = $car;
+	    }
 	}
 
 	public function delete($id = null) {
