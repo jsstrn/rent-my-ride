0ef5bab81ba066243c33a174ec5e17595440a87b
Mohafizz <mohafizz@gmail.com>
2013-12-21 17:36:08 +0800

    Validation for addition of comments

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,7 +2,26 @@
 
 class Post extends AppModel {
 
+	public $name = 'Post';
+	public $validate = array(
+		'title'=>array(
+			'title_must_not_be_blank'=>array(
+				'rule'=>'notEmpty',
+				'message'=>'This comment is missing a title!'
+			),
 
+			'title_must_be_unique'=>array(
+				'rule'=>'isUnique',
+				'message'=>'A comment with this title already exists!'
+				)
+			),
+		'body'=>array(
+			'body_must_not_be_blank'=>array(
+				'rule'=>'notEmpty',
+				'message'=>'This comment is missing its body!'
+				)
+			)
+		);
 }
 
 ?>
--- app/View/Posts/add.ctp
+++ app/View/Posts/add.ctp
@@ -1,7 +1,7 @@
 <h2>Add a Comment</h2>
 <?php
 	echo $this->Form->create('Post', array('action' => 'add'));
-	echo $this->Form->input('title');
-	echo $this->Form->input('body');
+	echo $this->Form->input('title',array('required'=>'false'));
+	echo $this->Form->input('body',array('required'=>'false'));
 	echo $this->Form->end('Create a Comment');
 ?>
