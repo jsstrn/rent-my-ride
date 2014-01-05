c9fa17913a7db81b3961c164f2bc031d3ee74bd4
AlnurFaisal <sn63625414@singnet.com.sg>
2014-01-05 21:58:23 +0800

    Modify routes codes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -47,4 +47,11 @@
  * the built-in default routes.
  */
 	require CAKE . 'Config' . DS . 'routes.php';
+
+	/* Paypal IPN plugin */
+	Router::connect('/paypal_ipn/process', array('plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'process'));
+	/* Optional Route, but nice for administration */
+	Router::connect('/paypal_ipn/:action/*', array('admin' => 'true', 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'index'));
+	/* End Paypal IPN plugin */
+	
 	//include(APP.'Plugin'.DS.'searchable'.DS.'config'.DS.'routes.php');
