326f3a9af637beab19543fd9a742ce669716b2f5
Mohafizz <mohafizz@gmail.com>
2014-01-08 08:26:40 +0800

    Small changes to like plugin

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -3,15 +3,8 @@
 class Comment extends AppModel{
 
 	public $name = 'Comment';
-
-<<<<<<< HEAD:app/Model/Comment.php
 	public $actsAs = array('Like.Likeable');
-
-=======
-	//public $actsAs = array('Like.Likeable');
 	//var $actsAs = array('Searchable.Searchable');
-	public $name = 'Post';
->>>>>>> 677119ff1fdd959dca0e13896db4258f164d4003:app/Model/Post.php
 	public $validate = array(
 		'title'=>array(
 			'title_must_not_be_blank'=>array(
