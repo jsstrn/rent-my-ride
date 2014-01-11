8f981b91a05d2f360889d84384a270ee5d2e37bb
ptb <ptbstudy@gmail.com>
2014-01-11 16:21:44 +0800

    upload reload

---
+++ app/Model/Upload.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* 
+*/
+class Upload extends AppModel {
+
+	/*
+	public function isUploadedFile($params) {
+	    $val = array_shift($params);
+	    if ((isset($val['error']) && $val['error'] == 0) ||
+	        (!empty( $val['tmp_name']) && $val['tmp_name'] != 'none')
+	    ) {
+	        return is_uploaded_file($val['tmp_name']);
+	    }
+	    return false;
+	}
+	*/
+}
+?>
---
+++ app/View/Uploads/edit.ctp
@@ -0,0 +1,1 @@
+<?php echo $this->Upload->edit('Company', $this->Form->fields['Company.id']); ?>
