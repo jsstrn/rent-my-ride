a3519eeb910c7615dda37203ae7c616a041fc847
jsstrn <jessternrays@gmail.com>
2014-01-26 00:58:55 +0800

    bugfix for uploads/add
    - files now upload to the directory
    - removed redundant images

--- app/Controller/UploadsController.php
+++ app/Controller/UploadsController.php
@@ -10,21 +10,20 @@ class UploadsController extends AppController {
 
 	public function add() {
 
+		$loggedIn = $this->Auth->user('id');
+
+		if (!$loggedIn) {
+			throw new NotFoundException(__('Please log in to upload your profile picture.'));
+		}
+
 		if ($this->request->is('POST')) {
 
-			$loggedIn = $this->Auth->user('id');
-
-			if (!$loggedIn) {
-				$this->Session->setFlash('Please log in to upload your profile picture.', 'flash/error');
-			} else {
-
-			}
-			
 			$dir = 'img/uploads/';
-			$path = $dir . basename($_FILES['uploader']['name']);
+			$tmp_name = $_FILES['uploader']['tmp_name'];
 			$name = $_FILES['uploader']['name'];
 			$type = $_FILES['uploader']['type'];
 			$size = $_FILES['uploader']['size'];
+			$path = $dir . basename($_FILES['uploader']['name']);
 
 			$this->request->data['Upload']['path'] = $path;
 			$this->request->data['Upload']['name'] = $name;
@@ -35,24 +34,30 @@ class UploadsController extends AppController {
 			$userExists = $this->Upload->findByUserId($loggedIn);
 
 			if (!$userExists) {
+
 				$this->Upload->create();
+				move_uploaded_file($tmp_name, $path);
+
 				if ($this->Upload->save($this->request->data)) {
 					$this->Session->setFlash('Your file has been uploaded!', 'flash/success');
 					$this->redirect(array('action' => 'index'));
+
 				} else {
 					$this->Session->setFlash('Unable to upload your profile picture. Try again later.', 'flash/error');
 				}
 			} else {
 
 				$this->Upload->id = $userExists['Upload']['id'];
+				move_uploaded_file($tmp_name, $path);
 
 				if ($this->Upload->save($this->request->data)) {
-				    $this->Session->setFlash(__('Your profile picture has been updated.', 'flash/success'));
+				    $this->Session->setFlash('Your profile picture has been updated.', 'flash/success');
 				    return $this->redirect(array('action' => 'index'));
+				} else {
+					$this->Session->setFlash(__('Unable to update your profile picture.', 'flash/error'));
 				}
-				$this->Session->setFlash(__('Unable to update your profile picture.', 'flash/error'));
 			}
 		}
 	}
-}
-?>
+
+}?>
