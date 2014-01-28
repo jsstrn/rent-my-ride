ed3992af83dc33bac6e3652d7057c3b9aea48760
AlnurFaisal <sn63625414@singnet.com.sg>
2014-01-29 01:12:57 +0800

    Remove uniqueness on comment title

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -17,11 +17,6 @@ class Comment extends AppModel{
 			'title_must_not_be_blank'=>array(
 				'rule'=>'notEmpty',
 					'message'=>'This comment is missing a title!'
-			),
-
-			'title_must_be_unique'=>array(
-				'rule'=>'isUnique',
-					'message'=>'A comment with this title already exists!'
 			)
 		),
 		'user_id'=>array(
